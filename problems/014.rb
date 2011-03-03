6bc9d52a4e22b7fa4535d230ddbe5dc034b988f4
Gene Hsu <github@hsufarm.com>
2011-03-03 12:03:28 -0800

    provided alternative implementation for 14, found bug in original implementation

--- problems/014.rb
+++ problems/014.rb
@@ -19,7 +19,7 @@
 
 def next_number(n)
   if n%2 == 0
-	n/2
+    n/2
   else
     3*n+1
   end
@@ -28,16 +28,16 @@ end
 max_length = 0
 max_n = 1
 sequence_lengths = { 1 => 1 }
-(2..1_000_000).each do |n|
+(1..1_000_000).each do |n|
   length = 1
   m = n
   while m != 1
     if sequence_lengths[m]
-      length += sequence_lengths[m]
+      length += sequence_lengths[m] - 1 # -1 is for not double counting current m
       m = 1
     else
-      m = next_number(m)
       length += 1
+      m = next_number(m)
     end
   end
   sequence_lengths[n] = length
@@ -47,3 +47,45 @@ sequence_lengths = { 1 => 1 }
   end
 end
 puts [ max_n, max_length ]
+
+class Collatz
+  @@cache = { 1 => 1 }
+
+  def self.sequence_length(n)
+    if n == 1
+      1
+    elsif @@cache[n]
+      @@cache[n]
+    elsif n.even? # 7% optimization
+      length = 0
+      while n.even?
+        n >>= 1
+        length += 1
+      end
+      length + sequence_length(n)
+    else
+      length = 1 + sequence_length(next_number(n))
+      @@cache[n] = length
+    end
+  end
+
+  def self.next_number(n)
+    if n.even?
+      n/2
+    else
+      3*n+1
+    end
+  end
+end
+
+max_length = 0
+max_n = 1
+(1..1_000_000).each do |n|
+  length = Collatz.sequence_length(n)
+  if length > max_length
+    max_n = n
+    max_length = length
+  end
+end
+puts [ max_n, max_length ]
+
